79532546a43a129e7f3b0855b25ca4a0e5b6b228
Bishara Korkor <bishara.korkor@gmail.com>
2016-05-25 16:27:02 -0700

    fuckin git

--- greyscale.py
+++ greyscale.py
@@ -2,11 +2,9 @@ import glob
 from PIL import Image
 
 i = 0
-for c in glob.glob("/home3/bkorkor/distracteddriver/data/train/*"):
-    print c
-    for imagefile in glob.glob(c + "/*"):
-        if i % 1000 == 0:
-            print i
-        i += 1
-        img = Image.open(imagefile)
-        img.convert('L').save(imagefile)
+for imagefile in glob.glob("/home3/bkorkor/distracteddriver/data/test/*"):
+    if i % 1000 == 0:
+        print i
+    i += 1
+    img = Image.open(imagefile)
+    img.convert('L').save(imagefile)
